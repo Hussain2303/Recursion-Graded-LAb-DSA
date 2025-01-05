8fa458b12d77ca24a411b44f1628102070d073cf
Hussain2303 <hussainajmal6780@gmail.com>
2025-01-06 01:19:54 +0500

    Chapa commit

---
+++ factorial.cpp
@@ -0,0 +1,156 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+int factorial(int n)
+{
+	if (n == 0)
+		return 1;
+	if (n > 0)
+		return n * factorial(n - 1);
+}
+int nod(int n)
+{
+	if (n == 0)
+		return 0;
+	if (n > 0)
+		return nod(n / 10) + 1;
+}
+int sod(int n)
+{
+	if (n == 0)
+		return 0;
+	if (n > 0)
+		return sod(n / 10) + n % 10;
+}
+int fab(int n)
+{
+	if (n == 1)
+		return 0;
+	if (n == 2)
+		return 1;
+	if (n > 2)
+		return fab(n - 1) + fab(n - 2);
+}
+int RD(int num, int reversedNum = 0)
+{
+	if (num == 0)
+		return reversedNum;
+	if (num > 0)
+		reversedNum = reversedNum * 10 + num % 10;
+	return RD(num / 10, reversedNum);
+}
+void DTB(int n)
+{
+	if (n > 0)
+	{
+		DTB(n / 2);
+		cout << n % 2;
+	}
+}
+int SumOF(int n)
+{
+	if (n > 0)
+		return n+ SumOF(n - 1);
+}
+void PN(int n)
+{
+	if (n == 0)
+		return;
+	if (n > 0)
+	{
+		return PN(n - 1);
+		cout << n << " ";
+	}
+}
+void reverseString(string& str, int start, int end)
+{
+	if (start >= end)
+		return;
+	else {
+		swap(str[start], str[end]);
+		reverseString(str, start + 1, end - 1);
+	}
+}
+int power(int a, int b)
+{
+	if (b == 0)
+		return 1;
+	if (b > 0)
+		return a * power(a, b - 1);
+}
+int gcd(int a, int b)
+{
+	if (b == 0)
+		return a;
+	if (b > 0)
+		return gcd (b, a % b);
+}
+void insertAtBottom(stack<int>& s, int element)
+{
+	if (s.empty()) {
+		s.push(element);
+		return;
+	}
+
+	// Pop the top element
+	int topElement = s.top();
+	s.pop();
+
+	// Recursively call to insert at the bottom
+	insertAtBottom(s, element);
+
+	// Push the top element back after recursion
+	s.push(topElement);
+}
+
+// Function to reverse the stack
+void reverseStack(stack<int>& s)
+{
+	if (s.empty()) {
+		return;
+	}
+
+	// Pop the top element
+	int topElement = s.top();
+	s.pop();
+
+	// Recursively reverse the remaining stack
+	reverseStack(s);
+
+	// Insert the popped element at the bottom of the reversed stack
+	insertAtBottom(s, topElement);
+}
+
+int main()
+{
+	/*string str = "hello";
+	cout << factorial(5) << endl;
+	cout << nod(5432) << endl;
+	cout << sod(5432) << endl;
+	cout << fab(5) << endl;
+	cout << RD(1024) << endl;
+	DTB(14);
+	cout << endl;
+	cout << SumOF(5) << endl;
+	 PN(10);
+	 reverseString(str,0,str.length()-1);
+	 cout << str << endl;
+	 cout << power(4, 3);
+	 cout << endl;
+	 cout << gcd(48, 18);*/
+	stack<int> s;
+	s.push(4);
+	s.push(3);
+	s.push(2);
+	s.push(1);
+
+	reverseStack(s);
+	while (!s.empty()) {
+		cout << s.top() << " ";
+		s.pop();
+	}
+	cout << endl;
+
+
+	return 0;
+}
